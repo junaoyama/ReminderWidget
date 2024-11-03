43a3b8ba8156afc14cafe8a32b24e4a7b908758f
Aoyama Jun <g.exst.aom.j@gmail.com>
2024-11-03 19:11:01 +0900

    最低限のiOSプロジェクトファイルを追加

---
+++ ReminderWidget/ReminderWidget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReminderWidget
+//
+//  Created by jun on 2024/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
---
+++ ReminderWidget/ReminderWidgetTests/ReminderWidgetTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderWidgetTests.swift
+//  ReminderWidgetTests
+//
+//  Created by jun on 2024/11/03.
+//
+
+import Testing
+@testable import ReminderWidget
+
+struct ReminderWidgetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
